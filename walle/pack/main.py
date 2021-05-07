0033c002f8d98a68adb2c1333625279a75df1520
张会朋 <zhanghuipeng@doushen.com>
2021-05-07 13:39:41 +0800

    增加readMe

--- walle/pack/main.py
+++ walle/pack/main.py
@@ -55,7 +55,6 @@ walleChannelPath = libPath + "walle-cli-all.jar"
 print('walle---获取lib下瓦力打包jar的路径-----' + walleChannelPath)
 
 # 这里是自定义输入路径
-
 channelsOutputFilePath = parentPath + "output"
 print('walle---channelsOutputFilePath-----' + channelsOutputFilePath)
 # 这里是获取多渠道打包配置信息
@@ -65,21 +64,10 @@ print('walle---channelFilePath-----' + channelFilePath)
 protectedSourceApkPath = parentPath + "apk" + getBackslash() + Config.protectedSourceApkName
 print('walle---protectedSourceApkPath-----' + protectedSourceApkPath)
 if os.access(protectedSourceApkPath, os.F_OK):
-    print('walle---protectedSourceApkPath---apk已经存在--' + protectedSourceApkPath)
+    print('walle---protectedSourceApkPath---apk存在--' + protectedSourceApkPath)
 else:
-    print('walle---protectedSourceApkPath---apk已经不存在--请确认根目录下的apk文件夹中是否有apk')
+    print('walle---protectedSourceApkPath---apk不存在--请确认根目录下的apk文件夹中是否有apk')
     exit(0)
-
-# 检查自定义路径，并作替换
-if len(Config.protectedSourceApkDirPath) > 0:
-    protectedSourceApkPath = Config.protectedSourceApkDirPath + getBackslash() + Config.protectedSourceApkName
-    print('walle---protectedSourceApkPath-----' + protectedSourceApkPath)
-if len(Config.channelsOutputFilePath) > 0:
-    channelsOutputFilePath = Config.channelsOutputFilePath
-    print('walle---channelsOutputFilePath-----' + channelsOutputFilePath)
-if len(Config.channelFilePath) > 0:
-    channelFilePath = Config.channelFilePath
-    print('walle---channelFilePath-----' + channelFilePath)
 
 
 def copyFile(srcFile, dstFile):
@@ -109,6 +97,7 @@ def cleanTempResource():
         pass
     except Exception as e:
         # 如果异常则打印日志
+        print('异常-清空临时资源-')
         print(e)
         pass
 
@@ -116,10 +105,11 @@ def cleanTempResource():
 # 清空渠道信息
 def cleanChannelsFiles():
     try:
-        os.makedirs(channelsOutputFilePath)
+        os.remove(channelsOutputFilePath)
         pass
     except Exception as e:
         # 如果异常则打印日志
+        print('异常-清空渠道信息-')
         print(e)
         pass
 
@@ -130,6 +120,7 @@ def createChannelsDir():
         os.makedirs(channelsOutputFilePath)
         pass
     except Exception as e:
+        print('异常-创建Channels输出文件夹-')
         print(e)
         pass
 
@@ -148,17 +139,18 @@ def delFile(path):
 # 先清除之前output文件夹中所有的文件
 if len(Config.channelsOutputFilePath) > 0:
     delFile(channelsOutputFilePath)
+# 清空Channels输出文件夹
+# cleanChannelsFiles()
 # 创建Channels输出文件夹
 createChannelsDir()
-# 清空Channels输出文件夹
-cleanChannelsFiles()
+
 
 # 对齐
 zipalignShell = buildToolsPath + "zipalign -v 4 " + protectedSourceApkPath + " " + zipalignedApkPath
 print('walle---zipalignShell-----' + zipalignShell)
 os.system(zipalignShell)
 
-# 签名
+# 签名 这里没有重新签名的过程
 # signShell = buildToolsPath + "apksigner sign --ks " + keystorePath + " --ks-key-alias " \
 #             + keyAlias + " --ks-pass pass:" + keystorePassword + " --key-pass pass:" \
 #             + keyPassword + " --out " + signedApkPath + " " + zipalignedApkPath
@@ -184,6 +176,6 @@ os.system(writeChannelShell)
 cleanTempResource()
 
 print("\n**** =============================执行完成=================================== ****\n")
-print("\n↓↓↓↓↓↓↓↓  Please check output in the path   ↓↓↓↓↓↓↓↓\n")
-print("\n" + channelsOutputFilePath + "\n")
+print("↓↓↓↓↓↓↓↓  Please check output in the path   ↓↓↓↓↓↓↓↓")
+print("" + channelsOutputFilePath + "")
 print("\n**** =============================执行完成=================================== ****\n")
